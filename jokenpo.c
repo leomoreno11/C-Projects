a3cf98579122b19169e5656376878ebb95b9db2f
Leonardo Moreno <67594083+leomoreno11@users.noreply.github.com>
2020-09-30 16:26:31 -0300

    Add files via upload

--- jokenpo.c
+++ jokenpo.c
@@ -13,100 +13,141 @@ F > P
 
 Leonardo Moreno Giantin Rodrigues
 RA: 21429414
+Victor Hugo Volpiano Fagundes
+RA: 21444928
+Mashara Arambasic Rebelo da Silva
+RA: 21467402
 */
 int main(){
-    int jogador1 = 0, jogador2 = 0, rodada = 0, w1 = 0, w2 = 0;
+    int rodada = 0, w1 = 0, w2 = 0;
     char jog1, jog2;
 
 
     //"letreiro"
     printf(" === JOKENPO === \n ");
-    printf("Regras:\n>P = Pedra\n>F = Papel\n>T = Tesoura\n>Melhor de 3!\n<");
+    printf("Regras:\n>P = Pedra\n>F = Papel\n>T = Tesoura\n>Melhor de 3!<\n");
 
-     do{
+    do // DO controlador limite de rodadas
+    {
+
+
+
     rodada = rodada + 1; //adiciona valor 1 em rodada, afim de controlar quantas rodadas ocorreram
     printf("----------------\n--- Rodada %i ---\n----------------\n\n", rodada);
 
-//Selecao de opcao jogador 1 com prevencao de erros
-
+ //Selecao de opcao jogador 1 com prevencao de erros
+    do{
     printf("Jogador 1: Pedra, Papel ou Tesoura ?\n");
     scanf(" %c", &jog1);
     jog1 = toupper(jog1); //transforma todos os caracteres em maiusculo
     printf(">Voce escolheu: %c.\n", jog1);
+        if (jog1 != 'P' && jog1 != 'T' && jog1 != 'F')
+        {
+            printf("\n>>Erro 001: caractere inválido. Tente novamente: \n");
+            printf(">P = Pedra\n>F = Papel\n>T = Tesoura\n");
+        }
+        
+    }
+    while (jog1 != 'P' && jog1 != 'T' && jog1 != 'F');
+    
+    
 
 
 //Selecao de opcao jogador 2 com prevencao de erros
 
+    do{
     printf("Jogador 2: Pedra, Papel ou Tesoura ?\n");
     scanf(" %c", &jog2);
     jog2 = toupper(jog2); //transforma todos os caracteres em maiusculo
     printf(">Voce escolheu: %c.\n", jog2);
+        if (jog2 != 'P' && jog2 != 'T' && jog2 != 'F')
+        {
+            printf("\n>>Erro 001: caractere inválido. Tente novamente: \n");
+            printf(">P = Pedra\n>F = Papel\n>T = Tesoura\n");
+        }
+        
+    }
+    while (jog2 != 'P' && jog2 != 'T' && jog2 != 'F');
 
 
-   //Checagem de quem ganhou + limitador de rodadas.
- // ifelse para definir empate antes do switch case para checar quem ganha ou perde
+        if (!(jog1==jog2))
+        {
+            switch (jog1)
+            {
+            case 'P':
+                if (jog2 == 'T')
+                {
+                    w1 = w1 + 1;
+                    printf("\n>>Jogador 1 venceu a %i rodada!<<\n", rodada);
+                    printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
+                }else
+                {
+                    w2 = w2 + 1;
+                    printf("\n>>Jogador 2 venceu a %i rodada!<<\n", rodada);
+                    printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
+                }
+                
+                
+                break;//case P
+            
+            case 'T':
+                if (jog2 =='F')
+                {
+                    w1 = w1 + 1;
+                    printf("\n>Jogador 1 venceu a %i rodada!\n", rodada);
+                    printf(">Pontuacao\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
+                }else
+                {
+                    w2 = w2 + 1;
+                    printf("\n>>Jogador 2 venceu a %i rodada!\n", rodada);
+                    printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);   
+                }
+                break;//case T
 
-if (!(jog1 == jog2)){
-   switch (jog1)
-   {
-   case 'P':
-       if (jog2 == 'T')
-       {
-        w1 = w1 + 1;
-        printf("\n>>Jogador 1 venceu a %i rodada!<<\n", rodada);
-        printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
-       }else{
-        w2 = w2 + 1;
-           printf("\n>>Jogador 2 venceu a %i rodada!<<\n", rodada);
-           printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
-       }
-       
-       break;
-   
-   case 'F':
-       if (jog2 == 'P')
-       {
-        w1 = w1 + 1;
-        printf("\n>>Jogador 1 venceu a %i rodada!<<\n", rodada);
-        printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
-       }else{
-        w2 = w2 + 1;
-           printf("\n>>Jogador 2 venceu a %i rodada!<<\n", rodada);
-           printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
-       }
-       
-       break;
-   
-   case 'T':
-       if (jog2 == 'F')
-       {
-        w1 = w1 + 1;
-        printf("\n>>Jogador 1 venceu a %i rodada!<<\n", rodada);
-        printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
-       }else{
-        w2 = w2 + 1;
-           printf("\n>>Jogador 2 venceu a %i rodada!<<\n", rodada);
-           printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
-       }
-       
-       break;
-   }
- }else{
-     printf("\n>>EMPATE!!<<\n Tente novamente.\n");
-     rodada = rodada - 1; //retrai uma rodada afim de manter a melhor de tres
- }
+            case 'F':
+                if (jog2 =='P')
+                {
+                    w1 = w1 + 1;
+                    printf("\n>>Jogador 1 venceu a %i rodada!<<\n", rodada);
+                    printf(">Pontuacao<\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);
+                }else
+                {
+                    w2 = w2 + 1;
+                    printf("\n>Jogador 2 venceu a %i rodada!\n", rodada);
+                    printf(">Pontuacao\n>Jogador 1: %i pontos.\n>Jogador 2: %i pontos.\n", w1, w2);      
+                }
+                break;//case F
+                
+            
+            default:
+                rodada = rodada - 1;
+                printf("\n>ERRO 001: caractere inválido!\n");
+                printf("Regras:\n>P = Pedra\n>F = Papel\n>T = Tesoura\n>Melhor de 3!\n<");
 
-    if( w1 == 2 || w2 == 2){
+                
+            }   
+
+
+            
+        }else//else IF JOG1==JOG2
+        {
+            rodada = rodada -1;
+            printf("\n\n>> EMPATE <<\nTente novamente!\n");
+        }//FIM else IF JOG1==JOG2
+        
+    if( w1 == 2 || w2 == 2){ 
         //adiciona valor 3 para rodadas para encerrar loop do while
         rodada = 3;
     }
 
-    }while(rodada != 3 ); 
-    // seleciona quem tem maior numero de vitorias e declara ganhador
+
+    }/*fim DO*/ while (rodada != 3); //limitador de rodadas. Cód para de rodar aqui
+    
     if (w1 > w2){
         printf("\n >> O jogador 1 ganhou o jogo!! <<\n");
     }else{
         printf("\n >> O jogador 2 ganhou o jogo!! <<\n");
     }
+
     return 0;
-}
+}// fim int main
